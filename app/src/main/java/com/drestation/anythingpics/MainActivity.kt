34c520a6823ebc3246c3dc6a423852f783d1c2bd
DreStation <drestation1@gmail.com>
2023-03-08 23:08:29 -0500

    Google sign in working

--- app/src/main/java/com/drestation/anythingpics/MainActivity.kt
+++ app/src/main/java/com/drestation/anythingpics/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.drestation.anythingpics
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.drestation.anythingpics.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // Enable binding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // More binding stuff
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //auth = FirebaseAuth.getInstance()
+
+        // Google sign-in stuff below
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Sign-in button press
+        binding.signInGoogleBtn.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            launcher.launch(signInIntent) // Call launcher
+        }
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                onSignInResult(task)
+            }
+        }
+
+    private fun onSignInResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            //val account: GoogleSignInAccount? = task.result
+            // Intent to next view
+            Toast.makeText(this, "IT WORKED", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
