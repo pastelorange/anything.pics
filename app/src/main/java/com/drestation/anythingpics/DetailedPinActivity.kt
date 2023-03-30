19e237ebb5d56de7d42b4bd9811d1b2ce33570c9
DreStation <drestation1@gmail.com>
2023-03-29 23:12:25 -0400

    can delete files from firebase storage too

--- app/src/main/java/com/drestation/anythingpics/DetailedPinActivity.kt
+++ app/src/main/java/com/drestation/anythingpics/DetailedPinActivity.kt
@@ -1,11 +1,11 @@
 package com.drestation.anythingpics
 
-import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.drestation.anythingpics.databinding.ActivityDetailedPinBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 
 class DetailedPinActivity : AppCompatActivity() {
@@ -16,24 +16,25 @@ class DetailedPinActivity : AppCompatActivity() {
         binding = ActivityDetailedPinBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Supporting all API versions (else is deprecated)
-        val pin = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableExtra("pin", Pin::class.java)
-        } else {
-            intent.getParcelableExtra<Pin>("pin")
-        }
+        // Get Pin object from previous activity
+        val pin = intent.getParcelableExtra<Pin>("pin")
 
+        // Populate elements
         binding.titleTextView.text = pin!!.title
         binding.captionTextView.text = pin.title
         Picasso.get().load(pin.imageUrl).into(binding.imageView)
 
         binding.deletePinButton.setOnClickListener {
-            // Query the database for pins made by the current user
+            // Delete pin from Firestore
             val db = FirebaseFirestore.getInstance().collection(pin.uid!!)
-            db.document(pin.documentId!!).delete().addOnSuccessListener {
-                Toast.makeText(this, "Pin deleted", Toast.LENGTH_SHORT).show()
-                finish()
-            }
+            db.document(pin.documentId!!).delete()
+
+            // Delete image file from Storage
+            val storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(pin.imageUrl!!)
+            storageRef.delete()
+
+            Toast.makeText(this, "Pin deleted", Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
